597f2ffdb0e8d70a7c8e840c18e98629b77d6158
Pragnya <prgnya@codeaurora.org>
2015-02-19 20:19:20 +0530

    Trebuchet: STK app rename customization for latam region.
    App name is read from SIM in STK app and sent via a broadcast.
    Name is operator specific. Launcher receives the broadcast,
    read the name and renames the STK app in all apps screen
    and workspace.
    
    Change-Id: I1251f1b985f392444ade082986c76fe6a23a7158
    CRs-Fixed: 797995

--- src/com/android/launcher3/LauncherApplication.java
+++ src/com/android/launcher3/LauncherApplication.java
@@ -17,6 +17,10 @@
 package com.android.launcher3;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 public class LauncherApplication extends Application {
     public static boolean LAUNCHER_SHOW_UNREAD_NUMBER;
@@ -25,6 +29,11 @@ public class LauncherApplication extends Application {
     public static boolean LAUNCHER_BACKUP_SHORTCUT_ENABLED;
     public static boolean LAUNCHER_MMX_SHORTCUT_ENABLED;
     public static boolean LAUNCHER_SFR_SHORTCUT_ENABLED;
+
+    private String mStkAppName = new String();
+    private final String STK_PACKAGE_INTENT_ACTION_NAME =
+            "org.codeaurora.carrier.ACTION_TELEPHONY_SEND_STK_TITLE";
+    private final String STK_APP_NAME = "StkTitle";
 
     @Override
     public void onCreate() {
@@ -42,6 +51,28 @@ public class LauncherApplication extends Application {
                 getResources().getBoolean(R.bool.config_srf_enabled);
         LauncherAppState.setApplicationContext(this);
         LauncherAppState.getInstance();
+        if (getResources().getBoolean(R.bool.config_launcher_stkAppRename)) {
+            registerAppNameChangeReceiver();
+        }
+    }
+
+    private void registerAppNameChangeReceiver() {
+        IntentFilter intentFilter = new IntentFilter(STK_PACKAGE_INTENT_ACTION_NAME);
+        registerReceiver(appNameChangeReceiver, intentFilter);
+    }
+
+    /**
+     * Receiver for STK Name change broadcast
+     */
+    private BroadcastReceiver appNameChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mStkAppName = intent.getStringExtra(STK_APP_NAME);
+        }
+    };
+
+    public String getStkAppName(){
+        return mStkAppName;
     }
 
     @Override
